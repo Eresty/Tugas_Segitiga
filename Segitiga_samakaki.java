c18d8a011b6ae71bae35033e1d1750805f9e6e29
rinaseptiana <rina.eresty@gmail.com>
2016-11-04 22:02:11 +0700

    Dan untuk membuat segitiga di java ini menggunakan perulangan FOR, perulangan FOR digunakan untuk mengulangi suatu perintah dengan jumlah pengulangan yang sudah diketahui. Sintaks penulisan perintah FOR adalah sebagai berikut ::
    FOR ( nilai_awal; nilai_akhir; penambahan/pengurangan) {
              pernyataan yang dijalankan
    	}

--- Segitiga_samakaki.java
+++ Segitiga_samakaki.java
@@ -15,7 +15,25 @@ public class Segitiga_samakaki {
         
         	System.out.println("\nCetak Segitiga Sama Kaki");
         	System.out.println("==========================");
+        	for (int i=1;i<=x;i++)
+			{
+				for(int j=x-1;j>=i;j--)
+				{ 
+					System.out.print(" ");
+				}
+				for(int k=1;k<=i;k++)
+				{ 
+					System.out.print("*");
+				}
+				for (int l=1;l<=i-1;l++)
+				{
+					System.out.print("*");
+				}
+				System.out.println();
+			}
         	
+        	
+        }   
     }
 }
     
